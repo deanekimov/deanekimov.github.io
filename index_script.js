1d6c77686dba0b33491b8ec8122ceb6494874101
deanekimov <148839031+deanekimov@users.noreply.github.com>
2024-10-28 01:31:41 -0500

    Trying random color on onclick function

--- index_script.js
+++ index_script.js
@@ -1,7 +1,15 @@
 function runAnimation(element) {
     element.style.animation = "content_div_inner_bordermove 1s";
     element.style.animationIterationCount = "1";
+    document.body.style.backgroundImage = `linear-gradient(to right, ${getRandomColor()}, ${getRandomColor()})`;
     element.addEventListener("animationend", function() {
         element.style.animation = "";
     });
 }
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
