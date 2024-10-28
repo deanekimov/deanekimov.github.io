23efe7b32d1f53950e2fe3e502f51ae5904b08d7
deanekimov <148839031+deanekimov@users.noreply.github.com>
2024-10-27 23:29:08 -0500

    testing for animation again

--- index_script.js
+++ index_script.js
@@ -2,6 +2,6 @@ function runAnimation(element) {
     element.style.animation = "content_div_inner_bordermove 1s";
     element.style.animationIterationCount = "1";
     element.addEventListener("animationend", function() {
-        element.style.animation = ""; // Reset animation
+        element.style.animation = "";
     });
 }
